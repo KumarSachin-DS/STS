d4fe42c5b1ddc947339653151232e407d771a88d
Sachin kumar <sachinkumar@192.168.1.2>
2025-08-27 00:49:16 -0700

    Initial commit for STS similarity API

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer, util
+
+app = FastAPI()
+model = SentenceTransformer('stsb-roberta-large')
+
+class TextPair(BaseModel):
+    text1: str
+    text2: str
+
+def similarity_score(text1, text2):
+    emb1 = model.encode(text1, convert_to_tensor=True)
+    emb2 = model.encode(text2, convert_to_tensor=True)
+    score = util.pytorch_cos_sim(emb1, emb2).item()
+    score = (score + 1) / 2  
+    return round(score, 3)
+
+@app.post("/")
+async def get_similarity(data: TextPair):
+    score = similarity_score(data.text1, data.text2)
+    return {"similarity score": score}
